544544110e96a24ecd887df0d8d32aac019e0bb3
03Matthew <npm.github.matthew03@gmail.com>
2022-04-30 17:33:47 +0200

    verision fix

--- src/main.js
+++ src/main.js
@@ -7,27 +7,7 @@ const setMaxBagSize = require('./utils/setMaxBagSize');
 
 const { Work, Slut, Rob, Crime } = require('./exports/earn');
 
-const init = async () => {
-    await Initialize(process.env.mongo);
 
-    await setMaxBagSize(100)
-
-    const a = new Crime({
-        userId: '1',
-        minWin: 10,
-        maxWin: 15,
-        minLose: 5,
-        maxLose: 10,
-        chance: 100
-    })
-
-    const { value, bagEccess, currentAmount, maxSize } = await a.getData()
-
-    await a.save()
-    console.log(value, bagEccess, currentAmount, maxSize)
-}
-
-init();
 
 module.exports = {
     Initialize,
